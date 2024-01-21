7f2ab46413ffffcb012fd4affa399ef980fc7a3e
riothung <mario.thung@gmail.com>
2024-01-22 00:44:00 +0800

    second commit

--- halaman_admin.php
+++ halaman_admin.php
@@ -6,7 +6,7 @@
     <title>TVRI Kupang</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"/>
     <link rel="stylesheet" href="halaman_admin.css"/>
-    <link rel="icon" href="Logo-Bawaslu-2018-Icon-PNG-HD.png"/>
+    <link rel="icon" href="assets/img/ntt.png"/>
   </head>
   <style>
     /* .body {
